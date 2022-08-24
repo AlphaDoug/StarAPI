12eda855e81cdc623d5accdea7a2fb0e96042fa9
AlphaDoug <519558938@qq.com>
2022-08-24 22:30:51 +0800

    1

---
+++ StarApiTool/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace StarApiTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChooseOriginBtn = new System.Windows.Forms.Button();
+            this.OriginFolderTxtBox = new System.Windows.Forms.TextBox();
+            this.TargetFolderTxtBox = new System.Windows.Forms.TextBox();
+            this.ChooseTargetBtn = new System.Windows.Forms.Button();
+            this.ExportBtn = new System.Windows.Forms.Button();
+            this.IsCopyResCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // ChooseOriginBtn
+            // 
+            this.ChooseOriginBtn.Location = new System.Drawing.Point(438, 25);
+            this.ChooseOriginBtn.Name = "ChooseOriginBtn";
+            this.ChooseOriginBtn.Size = new System.Drawing.Size(86, 32);
+            this.ChooseOriginBtn.TabIndex = 0;
+            this.ChooseOriginBtn.Text = "源文件夹";
+            this.ChooseOriginBtn.UseVisualStyleBackColor = true;
+            this.ChooseOriginBtn.Click += new System.EventHandler(this.ChooseOriginBtn_Click);
+            // 
+            // OriginFolderTxtBox
+            // 
+            this.OriginFolderTxtBox.Location = new System.Drawing.Point(74, 30);
+            this.OriginFolderTxtBox.Name = "OriginFolderTxtBox";
+            this.OriginFolderTxtBox.ReadOnly = true;
+            this.OriginFolderTxtBox.Size = new System.Drawing.Size(337, 23);
+            this.OriginFolderTxtBox.TabIndex = 1;
+            // 
+            // TargetFolderTxtBox
+            // 
+            this.TargetFolderTxtBox.Location = new System.Drawing.Point(74, 80);
+            this.TargetFolderTxtBox.Name = "TargetFolderTxtBox";
+            this.TargetFolderTxtBox.ReadOnly = true;
+            this.TargetFolderTxtBox.Size = new System.Drawing.Size(337, 23);
+            this.TargetFolderTxtBox.TabIndex = 3;
+            // 
+            // ChooseTargetBtn
+            // 
+            this.ChooseTargetBtn.Location = new System.Drawing.Point(438, 75);
+            this.ChooseTargetBtn.Name = "ChooseTargetBtn";
+            this.ChooseTargetBtn.Size = new System.Drawing.Size(86, 32);
+            this.ChooseTargetBtn.TabIndex = 2;
+            this.ChooseTargetBtn.Text = "目标文件夹";
+            this.ChooseTargetBtn.UseVisualStyleBackColor = true;
+            this.ChooseTargetBtn.Click += new System.EventHandler(this.ChooseTargetBtn_Click);
+            // 
+            // ExportBtn
+            // 
+            this.ExportBtn.Location = new System.Drawing.Point(74, 158);
+            this.ExportBtn.Name = "ExportBtn";
+            this.ExportBtn.Size = new System.Drawing.Size(450, 60);
+            this.ExportBtn.TabIndex = 4;
+            this.ExportBtn.Text = "导出";
+            this.ExportBtn.UseVisualStyleBackColor = true;
+            this.ExportBtn.Click += new System.EventHandler(this.ExportBtn_Click);
+            // 
+            // IsCopyResCheckBox
+            // 
+            this.IsCopyResCheckBox.AutoSize = true;
+            this.IsCopyResCheckBox.Location = new System.Drawing.Point(392, 122);
+            this.IsCopyResCheckBox.Name = "IsCopyResCheckBox";
+            this.IsCopyResCheckBox.Size = new System.Drawing.Size(132, 21);
+            this.IsCopyResCheckBox.TabIndex = 5;
+            this.IsCopyResCheckBox.Text = "是否复制Res文件夹";
+            this.IsCopyResCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 230);
+            this.Controls.Add(this.IsCopyResCheckBox);
+            this.Controls.Add(this.ExportBtn);
+            this.Controls.Add(this.TargetFolderTxtBox);
+            this.Controls.Add(this.ChooseTargetBtn);
+            this.Controls.Add(this.OriginFolderTxtBox);
+            this.Controls.Add(this.ChooseOriginBtn);
+            this.Name = "MainForm";
+            this.Text = "API转换工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button ChooseOriginBtn;
+        private TextBox OriginFolderTxtBox;
+        private TextBox TargetFolderTxtBox;
+        private Button ChooseTargetBtn;
+        private Button ExportBtn;
+        private CheckBox IsCopyResCheckBox;
+    }
+}
---
+++ StarApiTool/MainForm.cs
@@ -0,0 +1,312 @@
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace StarApiTool
+{        // 创建文件的方式
+    public enum CreateType
+    {
+        Overrite,
+        NewName,
+        DoNothing,
+    }
+    public partial class MainForm : Form
+    {
+        private string fromPath = string.Empty;
+        private string toPath = string.Empty;
+
+        public MainForm()
+        {
+            InitializeComponent();
+#if DEBUG
+            Environment.CurrentDirectory = Directory.GetCurrentDirectory() + "\\..\\..\\..\\";
+#endif
+
+        }
+
+        private void ChooseOriginBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            string pathName = dialog.SelectedPath;
+            OriginFolderTxtBox.Text = pathName;
+            fromPath = pathName;
+        }
+
+        private void ChooseTargetBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            string pathName = dialog.SelectedPath;
+            TargetFolderTxtBox.Text = pathName;
+            toPath = pathName;
+        }
+
+        private void ExportBtn_Click(object sender, EventArgs e)
+        {
+            //ConvertMD2Html(@"C:\Users\shang.ma\Documents\StarApi\UBaseInventoryComponent\UBaseInventoryComponent.md",
+            //  @"C:\Users\shang.ma\Documents\StarApi\UBaseInventoryComponent\UBaseInventoryComponentAUTO.html");
+
+            if (fromPath == string.Empty || toPath == string.Empty)
+            {
+                MessageBox.Show("请选择文件夹");
+                return;
+            }
+            if (!Directory.Exists(fromPath) || !Directory.Exists(toPath))
+            {
+                MessageBox.Show("路径不存在");
+                return;
+            }
+            var paths = getFiles(fromPath);
+            foreach (string path in paths)
+            {
+                if (path.EndsWith(".md"))
+                {
+                    //是MD文件，需要转化
+                    string to = path.Replace(".md", ".html");
+                    to = to.Replace(fromPath, toPath);
+                    ConvertMD2Html(path, to);
+                }
+            }
+            if (IsCopyResCheckBox.Checked)
+            {
+                //导出Res文件夹
+                CopyFileAndDir(fromPath + @"\Resources", toPath + @"\Resources");
+            }
+            MessageBox.Show("转化完成");
+        }
+
+        private void ConvertMD2Html(string fromPath = "", string toPath = "")
+        {
+            if (!File.Exists(toPath))
+            {
+                CreateFileOrDirectory(toPath);
+                //File.Create(toPath);
+            }
+            Console.WriteLine(fromPath + toPath);
+            string s = string.Format(@"--standalone --self-contained --css github.css {0} --output {1}", fromPath, toPath);
+            StartProcess(@"pandoc.exe", s);
+            ReplaceHtml(toPath);
+            return;
+        }
+
+       bool StartProcess(string runFilePath, string args)
+        {
+            args = args.Trim();
+            var output = "";
+            using (Process process = new Process())//创建进程对象    
+            {
+                process.StartInfo.FileName = runFilePath;  // 调用进程名
+                process.StartInfo.UseShellExecute = false;    //是否使用操作系统的shell启动
+                process.StartInfo.RedirectStandardInput = true;      //接受来自调用程序的输入
+                process.StartInfo.RedirectStandardOutput = true;     //由调用程序获取输出信息
+                process.StartInfo.CreateNoWindow = true;             //不显示调用程序的窗口 
+                process.StartInfo.RedirectStandardError = true;   //重定向标准错误输出
+                if (runFilePath != "cmd.exe")
+                {
+                    process.StartInfo.Arguments = args;
+                    process.Start(); // 后启动程序
+                }
+                else // 当直接调用cmd输入命令时传递参数的方法不一样
+                {
+                    process.Start(); // 先启动程序
+                    process.StandardInput.WriteLine(args + "&exit");
+                    process.StandardInput.AutoFlush = true;
+                }
+
+                output = process.StandardOutput.ReadToEnd(); // 这个是获取命令之后的返回值  可以不用
+                process.WaitForExit(); // 等待exe执行完成
+                //正常运行结束放回代码为0
+                if (process.ExitCode != 0)
+                {
+                    // 读取错误信息
+                    output = process.StandardError.ReadToEnd();
+                    output = output.ToString().Replace(Environment.NewLine, string.Empty);
+                    output = output.ToString().Replace("\n", string.Empty);
+                    MessageBox.Show(output);
+                    return false;
+                }
+                else
+                {
+                    output = process.StandardOutput.ReadToEnd();
+                }
+                process.Close();
+            }
+
+            return true;
+        }
+
+       void ReplaceHtml(string htmlPath)
+        {
+            Stream myStream = new FileStream(htmlPath, FileMode.Open);
+            //Encoding encode = Encoding.GetEncoding("gb2312");//若是格式为utf-8的需要将gb2312替换
+            StreamReader myStreamReader = new StreamReader(myStream);
+            string strhtml = myStreamReader.ReadToEnd();
+            string stroutput = Regex.Replace(strhtml, "<col style=\"width: (.*?)\" />", "");
+            stroutput = Regex.Replace(stroutput, @"\\", @"\");
+            myStream.Close();
+
+            myStream = new FileStream(htmlPath, FileMode.Create);
+            StreamWriter sw = new StreamWriter(myStream);
+            sw.Write(stroutput);
+            sw.Close();
+            myStream.Close();
+        }
+
+        /// <summary>
+        /// 获取最终需要的文件夹路径字符串集合
+        /// </summary>
+        /// <param name="folderName"></param>
+        /// <returns></returns>
+        private List<string> getFiles(string folderName, bool useOnlyName = false)
+        {
+            List<string> fileNameList = new List<string>() { };
+            DirectoryInfo theFolder = new DirectoryInfo(folderName);
+            DirectoryInfo[] dirInfo = theFolder.GetDirectories();
+
+            fileNameList = getFileList(dirInfo, fileNameList, useOnlyName);
+            FileInfo[] fileInfo = theFolder.GetFiles();
+            for (int i = 0; i < fileInfo.Length; i++)
+            {
+                if (useOnlyName)
+                {
+                    fileNameList.Add(fileInfo[i].Name);
+                }
+                else
+                {
+                    fileNameList.Add(fileInfo[i].FullName);
+                }
+            }
+            return fileNameList;
+        }
+
+        /// <summary>
+        /// 递归获取文件夹里的所有文件（包括子文件夹内的文件）
+        /// </summary>
+        /// <param name="dirInfos"></param>
+        /// <param name="fileNameList"></param>
+        /// <returns></returns>
+        private List<string> getFileList(DirectoryInfo[] dirInfos, List<string> fileNameList, bool useOnlyName = false)
+        {
+            foreach (DirectoryInfo sonFolder in dirInfos)
+            {
+                FileInfo[] fileInfo = sonFolder.GetFiles();
+                for (int i = 0; i < fileInfo.Length; i++)
+                {
+                    if (useOnlyName)
+                    {
+                        fileNameList.Add(fileInfo[i].Name);
+                    }
+                    else
+                    {
+                        fileNameList.Add(fileInfo[i].FullName);
+                    }
+
+                }
+                if (sonFolder.GetDirectories() != null)
+                {
+                    getFileList(sonFolder.GetDirectories(), fileNameList, useOnlyName);
+                }
+            }
+            return fileNameList;
+        }
+        public static void CreateFileOrDirectory(string path, CreateType createType = CreateType.DoNothing)
+        {
+            if (path.Contains("."))
+            {
+                FileInfo fileInfo = new FileInfo(path);
+
+                CreateFile(fileInfo, createType);
+            }
+            else
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+
+        public static void CreateFile(FileInfo fileInfo, CreateType createType = CreateType.DoNothing)
+        {
+            if (fileInfo.Exists)
+            {
+                switch (createType)
+                {
+                    case CreateType.Overrite:
+                        {
+                            fileInfo.Create().Close();
+                        }
+                        break;
+
+                    case CreateType.NewName:
+                        {
+                            string directoryName = fileInfo.DirectoryName;
+                            string fileName = fileInfo.Name;
+
+                            string[] fileNameAndSuffix = fileName.Split('.');
+                            string fileHead = fileNameAndSuffix[0];
+                            string suffix = fileNameAndSuffix[1];
+
+                            int i = 1;
+                            string newFilePath;
+                            do
+                            {
+                                string newFileName = $"{fileHead}_{i}.{suffix}";
+                                newFilePath = $@"{directoryName}\{newFileName}";
+
+                                i++;
+                            } while (File.Exists(newFilePath));
+
+                            File.Create(newFilePath).Close();
+                        }
+                        break;
+
+                    case CreateType.DoNothing:
+                        break;
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(fileInfo.DirectoryName);
+
+                // 创建完文件，记得关闭
+                fileInfo.Create().Close();
+            }
+        }
+
+        /// <summary>
+        /// 复制文件夹下的所有文件、目录到指定的文件夹
+        /// </summary>
+        /// <param name="dir">源文件夹地址</param>
+        /// <param name="desDir">指定的文件夹地址</param>
+        public static void CopyFileAndDir(string dir, string desDir)
+        {
+            if (!Directory.Exists(desDir))
+            {
+                Directory.CreateDirectory(desDir);
+            }
+            IEnumerable<string> files = Directory.EnumerateFileSystemEntries(dir);
+            if (files != null && files.Count() > 0)
+            {
+                foreach (var item in files)
+                {
+                    string desPath = Path.Combine(desDir, Path.GetFileName(item));
+
+                    //如果是文件
+                    var fileExist = File.Exists(item);
+                    if (fileExist)
+                    {
+                        //复制文件到指定目录下                     
+                        File.Copy(item, desPath, true);
+                        continue;
+                    }
+
+                    //如果是文件夹                   
+                    CopyFileAndDir(item, desPath);
+
+                }
+            }
+        }
+
+    }
+
+
+}
